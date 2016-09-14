d979e970c5865223d1978ed72e8352ae6ddf74f0
alios <abc.ua@yandex.ru>
2016-09-14 15:58:02 +0300

    up

---
+++ products-live-reload.js
@@ -0,0 +1,9 @@
+$(function() {
+    $.on('.table:eq(1) a[href^="?"]').click(function() {
+        var catalog = $(this).parents('.table').eq(0);
+
+        $.get('/shop/jsmodules/item/market.php' + location.search).then(function(html) {
+            $(catalog).replace(html);
+        });
+    });
+});
