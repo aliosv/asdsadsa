60678df110bd4236fe67f967c0c7f16f8d708328
alios <abc.ua@yandex.ru>
2016-09-14 16:29:31 +0300

    up

--- products-live-reload.js
+++ products-live-reload.js
@@ -4,8 +4,13 @@ $(function() {
 
         var $catalog = $(this).parents('.products-catalog').eq(0);
 
+        $catalog.addClass('products-catalog_pending');
+
         $.get('/shop/jsmodules/item/market.php' + $(this).attr('href')).then(function(html) {
             $catalog.replaceWith(html);
+        }, function() {
+            $catalog.replaceWith('<div class="products-catalog__error">Произошла ошибка, попробуйте перезагрузить страницу</div>');
+            $catalog.removeClass('products-catalog_pending');
         });
     });
 });
