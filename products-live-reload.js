f35e9b7f8fb13ccbb6fa10387411d6df764e8267
alios <abc.ua@yandex.ru>
2016-09-14 16:10:41 +0300

    up

--- products-live-reload.js
+++ products-live-reload.js
@@ -1,5 +1,7 @@
 $(function() {
-    $('.table:eq(1) a[href^="?"]').on('click', function() {
+    $('.table:eq(1) a[href^="?"]').on('click', function(e) {
+        e.preventDefault();
+        
         var catalog = $(this).parents('.table').eq(0);
 
         $.get('/shop/jsmodules/item/market.php' + location.search).then(function(html) {
